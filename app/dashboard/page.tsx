cb7aed16d0cd3c651011b8939b91644d8a56642b
vyomqode <vyom.thakkar@qodeinvest.com>
2026-02-06 12:56:54 +0530

    added day adjusted since inception to dinesh as well

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -826,6 +826,7 @@ const [returnViewType, setReturnViewType] = useState<"percent" | "cash">("percen
                 trailingReturns={convertedStats.trailingReturns}
                 drawdown={convertedStats.drawdown}
                 lastDate={lastDate}
+                adjustBenchmarkStartDate={true}
               />
             </div>
           </div>
