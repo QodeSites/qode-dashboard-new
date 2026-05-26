dbaccc7f968c370e1b20a4976d82ec1f5070fb15
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2026-05-26 15:25:30 +0530

    mangesh inactive enabled

--- app/lib/clients/mangesh.ts
+++ app/lib/clients/mangesh.ts
@@ -4,15 +4,16 @@ export const MANGESH_HIRVE_CONFIG = defineBifurcatedClient({
   name: "Mangesh Hirve",
   qcode: "QAC00064",
   schemes: {
-    "Scheme QAW++": {
-      inceptionDate: "2025-11-24",
-      exposure: "QAW++ Zerodha Total Portfolio",
-      profit:   "QAW++ Zerodha Total Portfolio",
-    },
     "Scheme QYE++": {
       inceptionDate: "2025-12-09",
       exposure: "QYE++ Zerodha Total Portfolio",
       profit:   "QYE++ Total Portfolio Value",
+    },
+    "Scheme QAW++": {
+      inceptionDate: "2025-11-24",
+      exposure: "QAW++ Zerodha Total Portfolio",
+      profit:   "QAW++ Zerodha Total Portfolio",
+      inactive: true,
     },
   },
 });
