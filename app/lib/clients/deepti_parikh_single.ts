8f67c86f316c1ab225349ff19000b0c2a0628308
akashghadgeqode <akash.ghadge@qodeinvest.com>
2026-06-05 15:21:56 +0530

    inception date fix

--- app/lib/clients/deepti_parikh_single.ts
+++ app/lib/clients/deepti_parikh_single.ts
@@ -4,7 +4,7 @@ export const DEEPTI_PARIKH_CONFIG = defineSingleStrategyClient({
   name: "Deepti Parikh",
   qcode: "QAC00022",
   strategyName: "Qode Yield Enhancer++",
-  inceptionDate: "2026-02-20",
+  inceptionDate: "2024-10-14",
   exposure: "QYE++ Zerodha Total Portfolio", // candidates: "QYE++ Total Portfolio Value", "QYE++ Zerodha Total Portfolio"
   profit: "QYE++ Total Portfolio Value", // candidates: "QYE++ Total Portfolio Value", "QYE++ Zerodha Total Portfolio"
 });
