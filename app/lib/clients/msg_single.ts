288e6d821b0acfb1f637b46170c2e53773d3fae5
akashghadgeqode <akash.ghadge@qodeinvest.com>
2026-06-29 13:54:44 +0530

    Added 6 Account

---
+++ app/lib/clients/msg_single.ts
@@ -0,0 +1,10 @@
+import { defineSingleStrategyClient } from "../bifurcated-client-builder";
+
+export const MSG_PARTNERS_LLP_CONFIG = defineSingleStrategyClient({
+  name: "MSG Partners LLP",
+  qcode: "QAC00116",
+  strategyName: "Qode All Weather++",
+  inceptionDate: "2025-06-18",
+  exposure: "QAW++ Zerodha Total Portfolio",
+  profit: "QAW++ Zerodha Total Portfolio",
+});
---
+++ app/lib/clients/raghav_single.ts
@@ -0,0 +1,10 @@
+import { defineSingleStrategyClient } from "../bifurcated-client-builder";
+
+export const RAGHVENDRA_SINGH_CONFIG = defineSingleStrategyClient({
+  name: "Raghvendra Singh",
+  qcode: "QAC00118",
+  strategyName: "Qode Yield Enhancer+",
+  inceptionDate: "2026-06-22",
+  exposure: "QYE+ Zerodha Total Portfolio",
+  profit: "QYE+ Total Portfolio Value",
+});
