0eba6020690ae6dd118f2853e9e683ec4b71da46
akashghadgeqode <akash.ghadge@qodeinvest.com>
2026-08-20 15:11:52 +0530

    anantroop added

---
+++ app/lib/clients/anantroop_single.ts
@@ -0,0 +1,10 @@
+import { defineSingleStrategyClient } from "../bifurcated-client-builder";
+
+export const ANANTROOP_FINANCIAL_ADVISORY_SERVICES_CONFIG = defineSingleStrategyClient({
+  name: "Anantroop Financial Advisory Services Private Limited",
+  qcode: "QAC00133",
+  strategyName: "Scheme QYE+",
+  inceptionDate: "2026-08-19",
+  exposure: "QYE+ Zerodha Total Portfolio",
+  profit: "QYE+ Total Portfolio Value",
+});
